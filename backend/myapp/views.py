127a6a854b2b9be8873b8d9c9ce3649a69750347
yangjijian <yangjijian@sensorsdata.cn>
2023-08-30 17:38:37 +0800

    feat: backend add lock to fix multi request bug

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,16 +1,20 @@
 import json
+import threading
+
 from django.http.response import JsonResponse
-from django.db import transaction
 from .models import Category, Task
+
+
+lock = threading.Lock()
 
 
 def get_all_data(request):
     cates = Category.objects.all()
-    data = [get_single_category_data(cate) for cate in cates]
+    data = {
+        "lists": [get_single_category_data(cate) for cate in cates],
+        "draggedItem": None
+    }
     return JsonResponse(data, safe=False)
-
-
-'[{"id": 1, "title": "TODO", "tasks": [{"id": 4, "title": "Learnning Kubernetes", "columnId": 1}]}, {"id": 2, "title": "DOING", "tasks": []}, {"id": 3, "title": "DONE", "tasks": []}]'
 
 
 def save_all_data(request):
@@ -25,13 +29,16 @@ def save_all_data(request):
                 task_obj = save_task(t, cate_obj)
                 tasks.append(task_obj)
 
-        Category.objects.all().delete()
-        Category.objects.bulk_create(cates)
-        Task.objects.bulk_create(tasks)
+        with lock:
+            Category.objects.all().delete()
+            Category.objects.bulk_create(cates)
+            Task.objects.bulk_create(tasks)
 
         cates = Category.objects.all()
-        data = [get_single_category_data(cate) for cate in cates]
-
+        data = {
+            "lists": [get_single_category_data(cate) for cate in cates],
+            "draggedItem": None
+        }
         return JsonResponse(data, safe=False)
 
     return JsonResponse([], safe=False)
@@ -53,7 +60,7 @@ def save_task(task_data: dict, cate: Category):
 def get_single_category_data(category: Category):
     return {
         "id": category.id,
-        "title": category.title,
+        "text": category.title,
         "tasks": [
             get_single_task_data(category, t) for t in category.tasks.all()
         ]
@@ -63,6 +70,6 @@ def get_single_category_data(category: Category):
 def get_single_task_data(category: Category, task: Task):
     return {
         "id": task.id,
-        "title": task.title,
+        "text": task.title,
         "columnId": category.id
     }
